47074514ad546eaebe7a8ef9930f68c5e7acf418
nikoms <nikoms@gmail.com>
2014-08-30 17:54:10 +0200

    Use of VfsStream

--- tests/CsvReaderTest.php
+++ tests/CsvReaderTest.php
@@ -4,23 +4,40 @@
 namespace Nikoms\FailLover;
 
 
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+
 class CsvReaderTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var vfsStreamDirectory
+     */
+    private $root;
+
+    public function setUp()
+    {
+        $this->root = vfsStream::setup('root',null, array(
+                'empty_file.csv',
+                'one_line.csv' => '"className","method","data","dataName"'
+            )
+        );
+    }
 
     public function testGetList_WhenFileDoesNotExist_TheListIsEmpty()
     {
-        $reader = new CsvReader(__DIR__ . '/unknow_file.csv');
+        $reader = new CsvReader($this->root->url() . '/unknow_file.csv');
         $this->assertCount(0, $reader->getList());
     }
 
     public function testGetList_WhenFileIsEmpty_TheListIsEmpty()
     {
-        $reader = new CsvReader(__DIR__ . '/files/empty_file.csv');
+        $reader = new CsvReader($this->root->url() . '/empty_file.csv');
         $this->assertCount(0, $reader->getList());
     }
 
     public function testGetList_WhenFileHasOneLine_ThereIsOneTest()
     {
-        $reader = new CsvReader(__DIR__ . '/files/one_line.csv');
+        $reader = new CsvReader($this->root->url() . '/one_line.csv');
         $list = $reader->getList();
         $this->assertCount(1, $list);
         $this->assertInstanceOf('Nikoms\FailLover\TestCaseResult\TestCaseInterface', $list[0]);
