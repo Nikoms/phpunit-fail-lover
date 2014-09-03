e326ecd08cf1e1484a8b8a933069e1f270b6d695
nikoms <nikoms@gmail.com>
2014-09-03 09:57:27 +0200

    Refactor the Listener

--- src/Listener/FailLoverListener.php
+++ src/Listener/FailLoverListener.php
@@ -2,55 +2,34 @@
 
 namespace Nikoms\FailLover\Listener;
 
+use Nikoms\FailLover\Command\ArgumentParser;
+use Nikoms\FailLover\TestCaseResult\RecorderInterface;
 use PHPUnit_Framework_AssertionFailedError;
 use PHPUnit_Framework_Test;
-use PHPUnit_Framework_TestSuite;
+
 
 class FailLoverListener extends \PHPUnit_Framework_BaseTestListener
 {
-    private $mustLogError = false;
-    private $filePath;
 
-    function __construct($filePath = '')
+    /**
+     * @var ArgumentParser
+     */
+    private $parser;
+    /**
+     * @var RecorderInterface
+     */
+    private $recorder;
+
+    function __construct(RecorderInterface $recorder)
     {
-        $this->filePath = $filePath;
-    }
-
-
-    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
-    {
-
-        $actions = array();
-        $commandLineAttributes = $_SERVER['argv'];
-        $positionOfPhpIniOptions = array_keys($commandLineAttributes, '-d');
-        $total = count($positionOfPhpIniOptions);
-        for ($i = 0; $i < $total; $i++) {
-            $option = $commandLineAttributes[$positionOfPhpIniOptions[$i]+1];
-            list($key, $value) = explode('=', $option);
-            if($key === 'fail-lover'){
-                $actions[$value] = true;
-            }
-        }
-
-        $this->mustLogError = isset($actions['log']);
+        $this->recorder = $recorder;
+        $this->parser = new ArgumentParser($_SERVER['argv']);
     }
 
     public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
     {
-        if($this->mustLogError && $test instanceof \PHPUnit_Framework_TestCase){
-            $this->log($test, $e, $time);
+        if ($this->parser->hasAction('log') && $test instanceof \PHPUnit_Framework_TestCase) {
+            $this->recorder->add($test);
         }
     }
-
-    private function log(\PHPUnit_Framework_TestCase $test, PHPUnit_Framework_AssertionFailedError $e, $time)
-    {
-        echo PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL;
-        $reflectionClass = new \ReflectionClass($test);
-        echo $reflectionClass->getName().'::'.$test->getName();
-        var_export(\PHPUnit_Util_Test::getProvidedData($reflectionClass->getName(), $test->getName(false)));
-        echo PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL;
-
-    }
-
-
 }
---
+++ tests/run/Listener/FailLoverListenerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Nikoms\FailLover\Listener;
+
+use Nikoms\FailLover\Tests\FilterTestMock;
+
+class FailLoverListenerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddFailure_WhenLogIsActive_RecorderIsCalled()
+    {
+        $this->assertInvocationWithParameters(array('-d', 'fail-lover=log'), $this->once());
+    }
+
+    public function testAddFailure_WhenLogIsNotActive_RecorderIsNotCalled()
+    {
+        $this->assertInvocationWithParameters(array(), $this->never());
+    }
+
+    private function assertInvocationWithParameters(
+        array $arguments,
+        \PHPUnit_Framework_MockObject_Matcher_Invocation $invocation
+    ) {
+        $_SERVER['argv'] = $arguments;
+        $recorder = $this->getMock('Nikoms\FailLover\TestCaseResult\RecorderInterface', array('add'));
+        $recorder->expects($invocation)->method('add');
+        $listener = new FailLoverListener($recorder);
+        $listener->addFailure(new FilterTestMock('testSimple'), new \PHPUnit_Framework_AssertionFailedError(), 0);
+    }
+}
+ 
