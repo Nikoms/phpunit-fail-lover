e941487de14e9e26fa02d90124079fc1524767e7
nikoms <nikoms@gmail.com>
2014-09-05 01:58:54 +0200

    Replace willReturn(..) by old fashion will($this->returnValue(...))
    So we are compatible with 3.7

--- tests/run/Filter/FilterTest.php
+++ tests/run/Filter/FilterTest.php
@@ -22,26 +22,32 @@ class FilterTest extends \PHPUnit_Framework_TestCase {
 
     public function testGetFilter_WhenThereIsNoTest_ItReturnsAnEmptyString()
     {
-        $this->reader->expects($this->any())->method('getList')->willReturn(array());
+        $this->reader->expects($this->any())->method('getList')->will($this->returnValue(array()));
         $filter = new Filter($this->reader);
         $this->assertSame('', (string) $filter);
     }
 
     public function testGetFilter_WhenThereIsOneTest_ItReturnsASimpleFilter()
     {
-        $this->reader->expects($this->any())->method('getList')->willReturn(array(
-                new TestCase('My\Class\Name','testMethod'),
-            ));
+        $this->reader->expects($this->any())->method('getList')->will($this->returnValue(
+                array(
+                    new TestCase('My\Class\Name','testMethod'),
+                )
+            )
+        );
         $filter = new Filter($this->reader);
         $this->assertSame('^My\\\\Class\\\\Name\:\:testMethod$', (string) $filter);
     }
 
     public function testGetFilter_WhenThereIsTwoTests_ItReturnsTwoFiltersSeparatedByAPipe()
     {
-        $this->reader->expects($this->any())->method('getList')->willReturn(array(
-                new TestCase('My\Class\Name','testMethod'),
-                new TestCase('My\Class\Name','testSimple'),
-            ));
+        $this->reader->expects($this->any())->method('getList')->will($this->returnValue(
+                array(
+                    new TestCase('My\Class\Name','testMethod'),
+                    new TestCase('My\Class\Name','testSimple'),
+                )
+            )
+        );
         $filter = new Filter($this->reader);
         $this->assertSame('^My\\\\Class\\\\Name\:\:testMethod$|^My\\\\Class\\\\Name\:\:testSimple$', (string) $filter);
     }
