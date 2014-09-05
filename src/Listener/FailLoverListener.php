144869f18035055c0ca0eb90fff7ac4b449bc684
nikoms <nikoms@gmail.com>
2014-09-05 20:50:21 +0200

    Log on error too

--- src/Listener/FailLoverListener.php
+++ src/Listener/FailLoverListener.php
@@ -2,6 +2,7 @@
 
 namespace Nikoms\FailLover\Listener;
 
+use Exception;
 use Nikoms\FailLover\Command\ArgumentParser;
 use Nikoms\FailLover\TestCaseResult\RecorderInterface;
 use PHPUnit_Framework_AssertionFailedError;
@@ -27,6 +28,19 @@ class FailLoverListener extends \PHPUnit_Framework_BaseTestListener
     }
 
     public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+        $this->log($test);
+    }
+
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        $this->log($test);
+    }
+
+    /**
+     * @param PHPUnit_Framework_Test $test
+     */
+    private function log(PHPUnit_Framework_Test $test)
     {
         if ($this->parser->hasAction('log') && $test instanceof \PHPUnit_Framework_TestCase) {
             $this->recorder->add($test);
