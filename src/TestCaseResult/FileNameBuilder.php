5f9dc1aaf691d6dbb4d18e65ad4712f49d896c92
nikoms <nikoms@gmail.com>
2014-09-10 14:39:43 +0200

    Add the possibility to give only a folder

--- src/TestCaseResult/FileNameBuilder.php
+++ src/TestCaseResult/FileNameBuilder.php
@@ -4,13 +4,19 @@
 namespace Nikoms\FailLover\TestCaseResult;
 
 
-class FileNameBuilder {
+class FileNameBuilder
+{
+
+    const BASIC_CSV_FILENAME = 'fail-lover.csv';
 
     public function create($pattern)
     {
-        $pattern = trim((string) $pattern);
-        if($pattern === ''){
-            return 'fail-lover.csv';
+        $pattern = trim((string)$pattern);
+        if ($pattern === '') {
+            return self::BASIC_CSV_FILENAME;
+        }
+        if (file_exists($pattern) && is_dir($pattern)) {
+            return $pattern . '/' . self::BASIC_CSV_FILENAME;
         }
     }
 }
--- tests/run/TestCaseResult/FileNameBuilderTest.php
+++ tests/run/TestCaseResult/FileNameBuilderTest.php
@@ -4,13 +4,36 @@
 namespace Nikoms\FailLover\TestCaseResult;
 
 
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+
 class FileNameBuilderTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var vfsStreamDirectory
+     */
+    private $root;
+
+    public function setUp()
+    {
+        $this->root = vfsStream::setup(
+            'root',
+            null,
+            array()
+        );
+    }
 
     public function testCreate_WhenPatternIsEmpty_ReturnAStaticFileName()
     {
         $builder = new FileNameBuilder();
         $this->assertSame('fail-lover.csv', $builder->create(''));
+    }
+
+    public function testCreate_WhenPatternIsAFolder_ReturnTheFolderWithTheStaticFileName()
+    {
+        $builder = new FileNameBuilder();
+        $folder = $this->root->url();
+        $this->assertSame($folder . '/fail-lover.csv', $builder->create($folder));
     }
 
 }
