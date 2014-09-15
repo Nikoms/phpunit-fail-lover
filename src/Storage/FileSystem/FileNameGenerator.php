17ced6ee44ba7386939767fc830c0753215b321b
Nikoms <nikoms@gmail.com>
2014-09-16 00:22:50 +0200

    Refactor FileNameGenerator

--- src/Storage/FileSystem/FileNameGenerator.php
+++ src/Storage/FileSystem/FileNameGenerator.php
@@ -26,11 +26,9 @@ class FileNameGenerator
 
         $newFileName = str_replace('{datetime}', date('Y-m-d-His'), $fileName);
         $newFileName = $this->replaceUniqId($newFileName);
-
         $newFileName = $this->replaceLastModifiedFile($newFileName);
 
         return $newFileName;
-
     }
 
     /**
@@ -40,62 +38,82 @@ class FileNameGenerator
      */
     private function replaceLastModifiedFile($filePath)
     {
-        return preg_replace_callback(
-            $this->getParameterizedPathRegex('last'),
+        return $this->replaceWithCallBack(
+            $filePath,
+            'last',
             function ($matches) {
-                $dir = rtrim($matches[1], '/') . '/';
-                $lastFile = FileNameGenerator::BASIC_FILENAME;
-                $lastModifiedTime = 0;
+                $dir = FileNameGenerator::addRightSlash($matches[1]);
 
-                if (file_exists($dir) && is_dir($dir) && $dh = opendir($dir)) {
-                    while (($file = readdir($dh)) !== false) {
-                        $currentFilePath = $dir . $file;
-                        if (
-                            $currentFilePath != '..'
-                            && $currentFilePath != '.'
-                            && is_file($currentFilePath)
-                            && $lastModifiedTime < filemtime($currentFilePath)
-                        ) {
-                            $lastFile = $file;
-                            $lastModifiedTime = filemtime($currentFilePath);
-                        }
-                    }
-                    closedir($dh);
-                } else {
-                    throw new \InvalidArgumentException($dir . ' is not a valid folder');
-                }
-
-                return $dir . $lastFile;
-
-            },
-            $filePath
+                return $dir . FileNameGenerator::getLastModifiedFile($dir);
+            }
         );
     }
+
+    public static function addRightSlash($dir)
+    {
+        return rtrim($dir, '/') . '/';
+    }
+
+    /**
+     * @param $dir
+     * @throws \InvalidArgumentException
+     * @return string
+     */
+    public static function getLastModifiedFile($dir)
+    {
+        $lastFile = '';
+        $lastModifiedTime = 0;
+
+        if (file_exists($dir) && is_dir($dir) && $dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                $currentFilePath = $dir . $file;
+                if (
+                    $currentFilePath != '..'
+                    && $currentFilePath != '.'
+                    && is_file($currentFilePath)
+                    && $lastModifiedTime < filemtime($currentFilePath)
+                ) {
+                    $lastFile = $file;
+                    $lastModifiedTime = filemtime($currentFilePath);
+                }
+            }
+            closedir($dh);
+
+            return $lastFile;
+        } else {
+            throw new \InvalidArgumentException($dir . ' is not a valid folder');
+        }
+    }
+
 
     /**
      * @param string $filePath
      * @return string
      */
-    public function replaceUniqId($filePath)
+    private function replaceUniqId($filePath)
     {
-        return preg_replace_callback(
-            $this->getParameterizedPathRegex('uniqId'),
-            function($matches){
-                $dir = rtrim($matches[1], '/') . '/';
-                return $dir . uniqid();
-            },
-            $filePath
+        return $this->replaceWithCallBack(
+            $filePath,
+            'uniqId',
+            function ($matches) {
+                return rtrim($matches[1], '/') . '/' . uniqid();
+            }
         );
-
-//        return str_replace('{uniqId}', uniqid(), $filePath);
     }
 
     /**
-     * @param $str
-     * @return string
+     * @param $filePath
+     * @param $param
+     * @param $function
+     * @return mixed
      */
-    public function getParameterizedPathRegex($str)
+    private function replaceWithCallBack($filePath, $param, $function)
     {
-        return '#\{([\w\/\.:]*):' . $str . '\}#';
+        return preg_replace_callback(
+            '#\{([\w\/\.:]*):' . $param . '\}#',
+            $function,
+            $filePath
+        );
     }
 }
+
