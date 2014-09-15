6a7eca44f21e501a6976903299202773b71bd2d1
Nikoms <nikoms@gmail.com>
2014-09-16 01:45:21 +0200

    Create a new FailLover shortcut listener

---
+++ src/Listener/FailLoverListener.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikoms
+ * Date: 16/09/2014
+ * Time: 00:51
+ */
+
+namespace Nikoms\FailLover\Listener;
+
+
+use Exception;
+use Nikoms\FailLover\Storage\FileSystem\Csv\CsvReader;
+use Nikoms\FailLover\Storage\FileSystem\Csv\CsvRecorder;
+use Nikoms\FailLover\Storage\FileSystem\FileNameGenerator;
+use PHPUnit_Framework_AssertionFailedError;
+use PHPUnit_Framework_Test;
+use PHPUnit_Framework_TestSuite;
+
+class FailLoverListener extends \PHPUnit_Framework_BaseTestListener
+{
+    /**
+     * @var LoggerListener
+     */
+    private $loggerListener;
+    /**
+     * @var LoggerListener
+     */
+    private $replayListener;
+
+    public function __construct($filePattern = 'fail-lover.csv')
+    {
+        $fileGenerator = new FileNameGenerator();
+        $fileName = $fileGenerator->create($filePattern);
+        $this->loggerListener = new LoggerListener(new CsvRecorder($fileName));
+        $this->replayListener = new ReplayListener(new CsvReader($fileName));
+    }
+
+    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->loggerListener->startTestSuite($suite);
+        $this->replayListener->startTestSuite($suite);
+    }
+
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+        $this->loggerListener->addError($test, $e, $time);
+    }
+
+    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+        $this->loggerListener->addFailure($test, $e, $time);
+    }
+}
